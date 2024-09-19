d59065bae137a1e10d1632b68be8dab5fbfdddb5
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-09-19 15:15:52 +0530

    created all required packages

---
+++ src/main/java/com/school/management/system/BookStoreRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.school.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreRestfulApiApplication.class, args);
+	}
+
+}
